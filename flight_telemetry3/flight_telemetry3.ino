45cbc4f43cb5642cca325a27680a1cd466f9125f
James Landritsi <xlandisx@gmail.com>
2012-06-03 00:20:09 -0400

    this time with xbee lovin

--- flight_telemetry3/flight_telemetry3.ino
+++ flight_telemetry3/flight_telemetry3.ino
@@ -1,72 +1,35 @@
-/***
-Project SAPHE
-Flight 2 Telemetry Software
+/* Project SAPHE */
 
-MicroSD reader
-SPI Pins
-D13 SCK
-D12 MISO
-D11 MOSI
-D8 CS
+#define ENABLE_SD
 
-BMP085
-3.3V to VCC
-GND to GND
-A4 to SDA
-A5 to SCL
-***/
-
-/*** Configuration ***/
-
-//#define ENABLE_SD
-#define ENABLE_BMP
-//#define ENABLE_CMP
-//#define ENABLE_ADXL
-
-/*** End Configuration ***/
-
-/* SD Card */
 #if defined(ENABLE_SD)
   #include <SD.h>
+  const int sdCS = 8;
+  const int sdELED = 6;
+  char filename[] = "LOG00.csv";
+  File logFile;
 #endif
 
-/* I2C */
-#if defined(ENABLE_BMP) || defined(ENABLE_ADXL) || defined(ENABLE_CMP)
-  #include <Wire.h>
-#endif
-
-/* Barometer */
-#if defined(ENABLE_BMP)
-  #include <Adafruit_BMP085.h>
-  Adafruit_BMP085 bmp;
-#endif
-
-/* Accelerometer */
-#if defined(ENABLE_ADXL)
-  #include <ADXL345.h>
-  ADXL345 accel;
-#endif
-
-/* Compass */
-#if defined(ENABLE_CMP)
-  #include <HMC5883L.h>
-  HMC5883L cmp;
-#endif
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   
-  /* Start I2C */
-  #if defined(ENABLE_BMP) || defined(ENABLE_ADXL) || defined(ENABLE_CMP)
-    Wire.begin();
-  #endif
-  
-  /* Barometer */
-  #if defined(ENABLE_BMP)
-    bmp.begin();
+  #if defined(ENABLE_SD)
+    pinMode(sdCS, OUTPUT);
+    pinMode(sdELED, OUTPUT);
+    if (!SD.begin(sdCS)) {
+      Serial.println("Card failed, or not present");
+      digitalWrite(sdELED, HIGH);
+      return;
+    }
+    else {
+      Serial.println("Card is initialized");
+      return;
+    }
   #endif
 }
 
-void loop() {
+void loop()
+{
   
 }
