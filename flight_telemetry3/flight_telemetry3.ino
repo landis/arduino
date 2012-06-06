38dfe1c04255f8736ebe8f6d560df6c3b5530bb7
James Landritsi <xlandisx@gmail.com>
2012-06-06 08:10:08 -0400

    not working

--- flight_telemetry3/flight_telemetry3.ino
+++ flight_telemetry3/flight_telemetry3.ino
@@ -3,12 +3,12 @@
 #define ENABLE_SD
 #define ENABLE_XB
 //#define ENABLE_SERIAL
-//#define ENABLE_BMP
-//#define ENABLE_RTC
+#define ENABLE_BMP
+#define ENABLE_RTC
 #define ENABLE_AXL
 #define ENABLE_CMP
 
-#include<stdlib.h>
+//#include<stdlib.h>
 
 #if defined(ENABLE_SD)
   #include <SD.h>
@@ -43,6 +43,12 @@
 #if defined(ENABLE_AXL)
   #include <ADXL345.h>
   ADXL345 accel;
+  int areConnected = 0;
+#endif
+
+#if defined(ENABLE_CMP)
+  #include <HMC5883L.h>
+  HMC5883L compass;
 #endif
 
 void setup()
@@ -98,8 +104,27 @@ void setup()
     rtc.begin();
   #endif
   
-  #if defined(ENABLE_AXL) || defined(ENABLE_CMP)
+  #if defined(ENABLE_AXL)
     Wire.begin();
+    accel.EnsureConnected();
+    accel = ADXL345(); //is this necessary?
+  #endif
+  
+  #if defined(ENABLE_CMP)
+    Wire.begin();
+    compass.EnsureConnected();
+    compass = HMC5883L(); //is this necessary?
+  #endif
+  
+  #if defined(ENABLE_CMP) && defined(ENABLE_AXL)
+    if(compass.IsConnected && accel.IsConnected)
+    {
+      areConnected = true;
+      compass.SetScale(1.3);
+      compass.SetMeasurementMode(Measurement_Continuous);
+      accel.SetRange(2, true);
+      accel.EnableMeasurements();
+    }
   #endif
   
   /* SD Card Log Header */
@@ -180,6 +205,21 @@ void loop()
       record += cmeters; record += ",";
     #endif
     
+    #if defined(ENABLE_AXL) && defined(ENABLE_CMP)
+      if(areConnected)
+      {
+        MagnetometerScaled magnetometerReadings = compass.ReadScaledAxis();
+        AccelerometerScaled accelerometerReadings = accel.ReadScaledAxis();
+        
+        float headingNTC = CalculateHeadingNotTiltCompensated(magnetometerReadings);
+        float headingTC = CalculateHeadingTiltCompensated(magnetometerReadings, accelerometerReadings);
+        
+        int heading = headingTC * 100;
+        record += heading; record += ",";
+      }
+    #endif
+      
+    
     if (SD.exists(filename)) {
         logFile = SD.open(filename, FILE_WRITE);
       logFile.println(record);
@@ -194,4 +234,61 @@ void loop()
   #endif
   
  delay(5000);
- }
+ 
+}
+
+float CalculateHeadingTiltCompensated(MagnetometerScaled mag, AccelerometerScaled acc)
+{
+  // We are swapping the accelerometers axis as they are opposite to the compass the way we have them mounted.
+  // We are swapping the signs axis as they are opposite.
+  // Configure this for your setup.
+  float accX = -acc.YAxis;
+  float accY = -acc.XAxis;
+  
+  float rollRadians = asin(accY);
+  float pitchRadians = asin(accX);
+  
+  // We cannot correct for tilt over 40 degrees with this algorthem, if the board is tilted as such, return 0.
+  if(rollRadians > 0.78 || rollRadians < -0.78 || pitchRadians > 0.78 || pitchRadians < -0.78)
+  {
+    return 0;
+  }
+  
+  // Some of these are used twice, so rather than computing them twice in the algorithem we precompute them before hand.
+  float cosRoll = cos(rollRadians);
+  float sinRoll = sin(rollRadians);  
+  float cosPitch = cos(pitchRadians);
+  float sinPitch = sin(pitchRadians);
+  
+  float Xh = mag.XAxis * cosPitch + mag.ZAxis * sinPitch;
+  float Yh = mag.XAxis * sinRoll * sinPitch + mag.YAxis * cosRoll - mag.ZAxis * sinRoll * cosPitch;
+  
+  float heading = atan2(Yh, Xh);
+    
+  return heading;
+}
+
+float CalculateHeadingNotTiltCompensated(MagnetometerScaled mag)
+{
+   // Calculate heading when the magnetometer is level, then correct for signs of axis.
+   float heading = atan2(mag.YAxis, mag.XAxis);
+   return heading;
+}
+
+float RadiansToDegrees(float rads)
+{
+  // Correct for when signs are reversed.
+  if(rads < 0)
+    rads += 2*PI;
+      
+  // Check for wrap due to addition of declination.
+  if(rads > 2*PI)
+    rads -= 2*PI;
+   
+  // Convert radians to degrees for readability.
+  float heading = rads * 180/PI;
+       
+  return heading;
+}
+#endif
+
